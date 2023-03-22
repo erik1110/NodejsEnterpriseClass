bca6b08a53ebd74450bb0a30e517cf5fe17b714c
erik1110 <oceanuheart@gmail.com>
2023-03-22 14:21:36 +0800

    [style] adjust space

--- daily-task/day2/index.ts
+++ daily-task/day2/index.ts
@@ -19,24 +19,24 @@ function correctTest(name: string) {
       }, 2000)
     })
   }
-  // 檢查獎勵
-  function checkReward(data: MyData) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if(data.score >= 90) {
-          resolve(`${data.name} 獲得電影票`);
-        } else if (data.score >= 60 && data.score < 90) {
-          resolve(`${data.name} 獲得嘉獎`);
-        } else {
-          reject(`您沒有獎品`)
-        }
-      }, 2000)
-    })
-  }
+// 檢查獎勵
+function checkReward(data: MyData) {
+return new Promise((resolve, reject) => {
+    setTimeout(() => {
+    if(data.score >= 90) {
+        resolve(`${data.name} 獲得電影票`);
+    } else if (data.score >= 60 && data.score < 90) {
+        resolve(`${data.name} 獲得嘉獎`);
+    } else {
+        reject(`您沒有獎品`)
+    }
+    }, 2000)
+})
+}
   
-  // 執行函式
-  /* 在此填寫答案*/
-  correctTest("小明")
+// 執行函式
+/* 在此填寫答案*/
+correctTest("小明")
     .then(data => checkReward(data as MyData))
     .then(name => console.log(name))
     .catch(error => console.log(error))
