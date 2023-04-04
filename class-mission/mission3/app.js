97b46972f7b41949bab08c1e9c72c48fa9b2f652
erik1110 <oceanuheart@gmail.com>
2023-04-05 00:43:41 +0800

    [feat] 調整 app.js 和新增 connections

--- class-mission/mission3/app.js
+++ class-mission/mission3/app.js
@@ -1,9 +1,3 @@
-// 資料庫設定開始
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/testPost4')
-    .then(res=> console.log("連線資料成功"));
-
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cors = require('cors');
@@ -15,10 +9,7 @@ var usersRouter = require('./routes/users');
 var postsRouter = require('./routes/posts');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+require('./connections')
 
 app.use(cors());
 app.use(logger('dev'));
@@ -30,25 +21,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-const port = 3000;
-app.listen(port, () => {
-  console.log(`應用程式正在監聽 ${port} 端口`);
-});
 
 module.exports = app;
